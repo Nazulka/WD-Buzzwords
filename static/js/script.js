e3664f684a5cd78177c54eed4c5ddd7b43dd6711
Nazulka <nazeekadiu@yahoo.co.uk>
2021-03-27 15:23:26 +0000

    Add Terms page html setup with Materialize FAB

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,14 @@
  $(document).ready(function(){
     $('.sidenav').sidenav({ edge: "right" });
     $('.tooltipped').tooltip();
+    $('.fixed-action-btn').floatingActionButton();
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+  });
+    
