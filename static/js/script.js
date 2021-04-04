65b5491bca787cf9cae855a495876e8f574004fd
Nazulka <nazeekadiu@yahoo.co.uk>
2021-04-04 19:57:50 +0000

    Add Icon Boxes section

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right" });
     $('input#term_name, textarea#term_description').characterCounter();
-    $('.parallax').parallax({ full_width: true });
 });
+
+// Slide
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    full_width: true,
+    indicators: false,
+    height: 400,
+    transition: 500,
+    interval: 6000
+});
+
 
 // Autocomplete
 const ac = document.querySelector('.autocomplete');
